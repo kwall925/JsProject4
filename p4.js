e99850eb3f65d2bb58e1ba73a6f3497b1c099133
kwall925 <kwall925@gmail.com>
2019-12-09 19:35:53 -0600

    linked to API,commented code,modified .txt file

--- p4.js
+++ p4.js
@@ -1,13 +1,17 @@
 const init = () => {
+  //make button a variable
   let searchButton = document.querySelector('#searchbutton');
-  let searchInput = document.querySelector('#searchinput').value;
-  searchButton.addEventListener('click', getData(searchInput));
+  //run API request on button submit
+  searchButton.onclick = getData;
 }
 
-async const getData = data => {
-  fetch('giphy.com').then()=>{
-    return response.json();
-  };
+const getData = () => {
+  //store user entered search query as variable
+  let searchInput = document.querySelector('#searchinput').value;
+  //define url of API
+  let url = `http://api.giphy.com/v1/gifs/search?q=${searchInput}&limit=10&api_key=RiXGFZpkcnIEB607G8uMPHxkuccKUtN6`;
+  //fetch request and response
+  fetch(url).then(response => console.log(response.json()));  
 }
 
 window.onload = init;
@@ -17,22 +21,7 @@ window.onload = init;
 
 
 
-
-
-
 /*
-i)
-Based on the search term, the application will render animated gifs
-from http://giphy.com/.
-
-//developers.giphy.com/dashboard/ that allows for api access
-//kwall925
-
-
-i) The "Search" button will search http://giphy.com based on the user-entered search term.
-The goal of the search is to fetch a promise using the url
-http://api.giphy.com/v1/gifs/search?q=funny cats&limit=10&api_key=your api key
-
 
 i) Once you have retrieved the promise, then you should parse the results using JSON.
 The json results should then be iterated over using the .map function
@@ -42,4 +31,4 @@ to display a list of the 10 image results that came back from the search results
 i) You cannot use a library for this project. All code must be completed using
 modern javascript syntax. i.e. modern function syntax using string templating
 instead of string concatenation,
-(map/reduce/filter) instead of loops
+(map/reduce/filter) instead of loops */
