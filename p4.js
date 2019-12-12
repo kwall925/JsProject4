74e3392894a87e3bdb292aad978381862e6c868b
kwall925 <kwall925@gmail.com>
2019-12-11 19:41:23 -0600

    fixed collapsable wrapper, improved page layout

--- p4.js
+++ p4.js
@@ -12,6 +12,8 @@ const init = () => {
 const getData = () => {
   //store user entered search query as variable
   let searchInput = document.querySelector('#searchinput').value;
+  document.querySelector('wrapper').style.height = 'inherit';
+  document.querySelector('wrapper').style.overflow = 'auto';
   //define url of API
   let url = `http://api.giphy.com/v1/gifs/search?q=${searchInput}&limit=10&api_key=RiXGFZpkcnIEB607G8uMPHxkuccKUtN6`;
   //fetch request and response
@@ -20,15 +22,11 @@ const getData = () => {
        data = data.images.downsized.url;
        appendImg(data);
     });
+    let searchButton = document.querySelector('#searchbutton');
+    searchButton.onclick = repeatData;
+    clear();
     console.log(gifData);
-    /*for(let i = 0; i < gifData.length; i++){
-    let theGif = document.createElement("img");
-    theGif.src = gifData[i];
-    theGif.style.marginTop = "2rem";
-    document.querySelector('#giphy').appendChild(theGif);
-    }*/
-   // insertGif.innerHTML = insertGif;
-    console.log(response.data);
+    console.log(response.data);    
   });
 }
 
@@ -37,6 +35,15 @@ const appendImg = gif => {
   theGif.src = gif;
   theGif.style.marginTop = "2rem";
   document.querySelector('#giphy').appendChild(theGif);
+}
+
+const clear = () => {
+  document.querySelector('#searchinput').value = ' ';
+}
+
+const repeatData = () =>{
+  document.querySelector('#giphy').innerHTML = "";
+  getData();
 }
 
  /* const addImg = () => {
@@ -50,29 +57,3 @@ const appendImg = gif => {
 
 window.onload = init;
 
-//(response => console.log(response.json()));  
-//}
-
-
-/*    let gifTen = gifData.map(function(gif) {
-      document.querySelector('#giphy').innerHTML = gif;
-    });*/
-
-
-/* fetch(url).then(response => {let myObj = response.json();
-  console.log(myObj);*/
-
-
-  //  fetch(url).then(response => response.json()).then(response => console.log(response.data));
-
-/*
-
-i) Once you have retrieved the promise, then you should parse the results using JSON.
-The json results should then be iterated over using the .map function
-to display a list of the 10 image results that came back from the search results.
-
-
-i) You cannot use a library for this project. All code must be completed using
-modern javascript syntax. i.e. modern function syntax using string templating
-instead of string concatenation,
-(map/reduce/filter) instead of loops */
