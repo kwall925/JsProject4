dcecc8189e64f968a7447ea1d1ee18f13782bd5e
kwall925 <kwall925@gmail.com>
2019-12-12 10:23:30 -0600

    groomed & commented code. max-width to wrapper

--- p4.js
+++ p4.js
@@ -1,35 +1,31 @@
+  /*run API request on button submit*/
 const init = () => {
-  //make button a variable
   let searchButton = document.querySelector('#searchbutton');
-  //run API request on button submit
   searchButton.onclick = getData;
-  //make heading a variable
-  let h1 = document.querySelector('h1');
-  //add header underline on hover
- // h1.onmouseover = addImg;
 }
 
 const getData = () => {
-  //store user entered search query as variable
+  /*store user entered search query as variable*/
   let searchInput = document.querySelector('#searchinput').value;
   document.querySelector('wrapper').style.height = 'inherit';
   document.querySelector('wrapper').style.overflow = 'auto';
-  //define url of API
+  /*define url of API*/
   let url = `http://api.giphy.com/v1/gifs/search?q=${searchInput}&limit=10&api_key=RiXGFZpkcnIEB607G8uMPHxkuccKUtN6`;
-  //fetch request and response
+  /*fetch request and response*/
   fetch(url).then(response => response.json()).then(response => {
     let gifData = response.data.map((data) => {
        data = data.images.downsized.url;
        appendImg(data);
     });
+    /*run another search on button click*/
     let searchButton = document.querySelector('#searchbutton');
     searchButton.onclick = repeatData;
+    /*clear search bar*/ 
     clear();
-    console.log(gifData);
-    console.log(response.data);    
   });
 }
 
+/*create element and append returned img data*/
 const appendImg = gif => {
   let theGif = document.createElement("img");
   theGif.src = gif;
@@ -37,23 +33,16 @@ const appendImg = gif => {
   document.querySelector('#giphy').appendChild(theGif);
 }
 
+  /*clear search bar*/
 const clear = () => {
   document.querySelector('#searchinput').value = ' ';
 }
 
+  /*clear existing gifs and display new from search*/
 const repeatData = () =>{
   document.querySelector('#giphy').innerHTML = "";
   getData();
 }
 
- /* const addImg = () => {
-      //create empty img element
-      let underline = document.createElement('img');
-      //style and append underline
-      underline.src = "images/underline.gif";
-      underline.style.width = "40%";
-      document.querySelector('h1').appendChild(underline);
-    }*/
-
+/*run top funtion on page load*/
 window.onload = init;
-
